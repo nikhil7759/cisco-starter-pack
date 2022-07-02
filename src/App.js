4b12d6a2356b51c61a5caaef647cfcf25767b6d0
nikhil7759 <nikhil8434@gmail.com>
2022-07-02 21:28:55 +0530

    Initial commit

---
+++ src/App.js
@@ -0,0 +1,25 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <img src={logo} className="App-logo" alt="logo" />
+        <p>
+          Hey there it's me Nikhil
+        </p>
+        <a
+          className="App-link"
+          href="https://reactjs.org"
+          target="_blank"
+          rel="noopener noreferrer"
+        >
+          Learn React with me
+        </a>
+      </header>
+    </div>
+  );
+}
+
+export default App;
